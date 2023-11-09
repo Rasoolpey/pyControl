afeefb9309edda12472d38e5a8aa28d1e58901de
Rasool Peykarporsan <pvm8318@autuni.ac.nz>
2023-11-09 16:02:32 +1300

    updating non-linear

--- non-linear equations.py
+++ non-linear equations.py
@@ -1,4 +1,4 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -6,3 +6,11 @@ fig = plt.figure(1, figsize=(5,5))
 
 delta = 0.025
 x,y = np.meshgrid(np.arange(-4,4.1,delta),np.arange(-4,4.1,delta))
+print(x,y)
+
+f1 = x**2 + y-5
+f2 = x**2 + y**2 - 7
+
+plt.contour(x,y,f1,[0])
+plt.contour(x,y,f2,[0])
+plt.show()
